18c50d177f12fccc8f1dd9fd0c54f3fe8b85f773
zhangyong <758922096@qq.com>
2024-03-18 17:45:20 +0800

    feat: 修改获取完整图片url

--- downImage.js
+++ downImage.js
@@ -10,13 +10,12 @@ export async function downloadAndReplaceImages(articleContent, opt) {
   // 匹配两种URL
   // <img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/a747e579e38246eb8700400da4c0e3bf~tplv-k3u1fbpfcp-zoom-1.image" alt="">
   // ![前端工程化](https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/16efed95c98f4b669d3b6feb80e45f9e~tplv-k3u1fbpfcp-watermark.image)
-  // 定义正则表达式来匹配<img>标签中的图片URL，仅匹配特定前缀的地址
-  const imgTagRegex = /<img.*?src=["'](https:\/\/p3-juejin\.byteimg\.com[^"']*?)["'].*?>/g;
+  // 定义正则表达式来匹配包含 "juejin" 字符串且以图片格式结尾的 URL
+  // const imgUrlRegex = /https?:\/\/.*?juejin.*?\.(?:png|jpg|jpeg|gif|bmp|webp|image)/gi;
+  const imgUrlRegex = /https?:\/\/.*?juejin.*?\.(?:png|jpg|jpeg|gif|bmp|webp|image)(?:\?.*?)?/gi;
 
-  // 提取所有<img>标签中的图片URL
-  const imgSrcMatches = [...articleContent.matchAll(imgTagRegex)];
-  const imgSrcList = imgSrcMatches.map((match) => match[1]);
-
+  // 提取所有匹配的图片 URL
+  const imgSrcList = articleContent.match(imgUrlRegex) || [];
   // 创建目标文件夹
   if (!fs.existsSync(assetsFolderPath)) {
     fs.mkdirSync(assetsFolderPath, { recursive: true });
@@ -24,15 +23,15 @@ export async function downloadAndReplaceImages(articleContent, opt) {
 
   // 辅助函数：生成唯一的文件名
   function generateUniqueFilename(url, existingFiles) {
-    const uniqueIdMatch = url.match(/[^\/]+(?=\.image)/); // 提取URL中的唯一标识符
+    const uniqueIdMatch = url.match(/[^\/]+(?=\.(?:png|jpg|jpeg|gif|bmp|webp|image))/i); // 提取URL中的唯一标识符
     let uniqueId = uniqueIdMatch ? uniqueIdMatch[0] : "";
     uniqueId = uniqueId.replace(/[^\w]+/g, "_"); // 替换非单词字符为下划线
 
-    let filename = uniqueId + ".jpg"; // 使用 jpg 格式
+    let filename = `${opt.index}_${uniqueId}.jpg`; // 使用 jpg 格式
     let suffix = 1;
 
     while (existingFiles.includes(filename)) {
-      filename = `${uniqueId}_${suffix}.jpg`;
+      filename = `${opt.index}_${uniqueId}_${suffix}.jpg`;
       suffix++;
     }
 
@@ -41,6 +40,9 @@ export async function downloadAndReplaceImages(articleContent, opt) {
 
   // 下载图片到本地并转换格式
   async function downloadImage(imgSrc) {
+    // 去除 URL 中的问号及其后的内容
+    imgSrc = imgSrc.replace(/\?.*$/, "");
+
     const response = await axios({
       url: imgSrc,
       method: "GET",
@@ -61,8 +63,11 @@ export async function downloadAndReplaceImages(articleContent, opt) {
 
   // 下载所有图片并替换文章中的URL
   const localImagePaths = await Promise.all(imgSrcList.map(downloadImage));
-  localImagePaths.forEach((contentImagePath, index) => {
-    articleContent = articleContent.replace(imgSrcList[index], contentImagePath);
+  imgSrcList.forEach((imgSrc, index) => {
+    // console.log('imgSrc',imgSrc)
+    // console.log('localImagePath',localImagePaths[index])
+    // 在 articleContent 中查找并替换对应的图片 URL
+    articleContent = articleContent.replace(imgSrc, localImagePaths[index]);
   });
 
   return articleContent;
